7a215a844de4222303c31ef7174f50bb0ce16a5a
hyotatoFrappuccino <hjkiln@naver.com>
2025-06-13 11:12:02 +0900

    fix: LocalDate의 Validation이 @NotBlank로 되어있는 사항 수정. -> @NotNull

--- src/main/java/com/studiop/dormmanagementsystem/entity/dto/PaymentUpdateRequest.java
+++ src/main/java/com/studiop/dormmanagementsystem/entity/dto/PaymentUpdateRequest.java
@@ -5,6 +5,7 @@ import com.studiop.dormmanagementsystem.entity.enums.PaymentType;
 import com.studiop.dormmanagementsystem.validation.EnumValue;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
@@ -13,7 +14,7 @@ public record PaymentUpdateRequest(
         String name,
         @Min(0)
         Integer amount,
-        @NotBlank
+        @NotNull
         LocalDate date,
         @EnumValue(enumClass = PaymentStatus.class, message = "존재하지 않는 유형입니다.")
         String status,
--- src/main/java/com/studiop/dormmanagementsystem/entity/dto/RoundRequest.java
+++ src/main/java/com/studiop/dormmanagementsystem/entity/dto/RoundRequest.java
@@ -1,15 +1,16 @@
 package com.studiop.dormmanagementsystem.entity.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record RoundRequest(
         @NotBlank
         String name,
-        @NotBlank
+        @NotNull
         LocalDate startDate,
-        @NotBlank
+        @NotNull
         LocalDate endDate,
         @NotBlank
         String password
