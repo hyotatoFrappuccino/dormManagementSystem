01e1e326c9e5fc7b8421e5bbcb776be2ebce7189
hyotatoFrappuccino <66837740+hyotatoFrappuccino@users.noreply.github.com>
2025-10-01 11:38:03 +0900

    fix: 스프링부트 테스트 제거 (#11)

--- src/test/java/com/studiop/dormmanagementsystem/DormManagementSystemApplicationTests.java
+++ src/test/java/com/studiop/dormmanagementsystem/DormManagementSystemApplicationTests.java
@@ -1,9 +1,7 @@
 package com.studiop.dormmanagementsystem;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DormManagementSystemApplicationTests {
 
     @Test
